4cfdfeb6679f7706da89df1a9eee0d3089071aa3
Khutso95 <47758109+Khutso95@users.noreply.github.com>
2020-06-30 01:38:52 +0200

    api

--- Code.js
+++ Code.js
@@ -45,77 +45,139 @@ function MobileNav() {
   }
 }
 //Face Recognition
-var UploadedImage = document.querySelector(".UploadedImage");
+// var UploadedImage = document.querySelector(".UploadedImage");
+// const ImageSection = document.querySelector(".ImageSection");
+// Promise.all([
+//   faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+//   faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+//   faceapi.nets.ssdMobilenetv1.loadFromUri("/models"),
+// ]).then(Compute);
+
+// async function Compute() {
+//   const FaceContainer = document.createElement("div");
+//   FaceContainer.style.position = "relative";
+//   ImageSection.append(FaceContainer);
+
+//   const LabeledFaces = await LoadingIndividualTestImages();
+//   // console.log(LabeledFaces);
+//   const MatchedFaces = new faceapi.FaceMatcher(LabeledFaces, 0.65);
+//   ImageSection.append("Loaded");
+
+//   let image;
+//   let FaceSquares;
+
+//   UploadedImage.addEventListener("change", async () => {
+//     if (image) {
+//       image.remove();
+//     }
+//     if (FaceSquares) {
+//       FaceSquares.remove();
+//     }
+//     image = await faceapi.bufferToImage(UploadedImage.files[0]);
+//     FaceSquares = faceapi.createCanvasFromMedia(image);
+//     ImageSection.append(image);
+//     ImageSection.append(FaceSquares);
+//     const theSizes = { width: image.width, height: image.height };
+//     faceapi.matchDimensions(FaceSquares, theSizes);
+
+//     const detected = await faceapi
+//       .detectAllFaces(image)
+//       .withFaceLandmarks()
+//       .withFaceDescriptors();
+
+//     const resizedSquares = faceapi.resizeResults(detected, theSizes);
+
+//     const results = resizedSquares.map((d) =>
+//       FaceMatcher.findBestMatch(d.descriptor)
+//     );
+
+//     results.forEach((result, i) => {
+//       const square = resizedSquares[i].detection.box;
+//       const drawSquares = new faceapi.draw.DrawBox(square, {
+//         label: result.toString(),
+//       });
+//       drawSquares.draw(FaceSquares);
+//     });
+//   });
+// }
+
+// // var xmlhttp = new XMLHttpRequest();
+// // var url =
+// //   "https://github.com/Khutso95/WSOA3028A_884200/tree/master/test_individial_images";
+// // xmlhttp.open("GET", url, false);
+// // xmlhttp.setRequestHeader("Access-Control-Allow-Origin:", "*");
+// // xmlhttp.send();
+// faceapi.env.monkeyPatch({
+//   fetch: fetch,
+//   Canvas: window.HTMLCanvasElement,
+//   Image: window.HTMLImageElement,
+//   ImageData: canvas.ImageData,
+//   createCanvasElement: () => document.createElement("canvas"),
+//   createImageElement: () => document.createElement("img"),
+// });
+// function LoadingIndividualTestImages() {
+//   const names = [
+//     "Angela Bassett",
+//     "Danai Gurira",
+//     "Letitia Wright",
+//     "Lupita Nyong'o",
+//   ];
+//   return Promise.all(
+//     names.map(async (label) => {
+//       const Faces = [];
+//       for (let i = 1; i <= 3; i++) {
+//         const pics = faceapi.fetchImage(
+//           `https://cors-anywhere.herokuapp.com/https://github.com/Khutso95/WSOA3028A_884200/tree/master/test_individial_images/${label}/${i}.jpg`
+//         );
+//         const detectedFaces = await faceapi
+//           .detectSingleFace(pics)
+//           .withFaceLandmarks()
+//           .withFaceDescriptor();
+//         Faces.push(detectedFaces.descriptor);
+//       }
+//       return new faceapi.LabeledFaceDescriptors(label, Faces);
+//     })
+//   );
+// }
 const ImageSection = document.querySelector(".ImageSection");
+
+const myfeed = document.getElementById("myfeed");
+// myfeed.append(ImageSection);
 Promise.all([
-  faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+  faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
   faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
-  faceapi.nets.ssdMobilenetv1.loadFromUri("/models"),
-]).then(Compute);
+  faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+  faceapi.nets.faceExpressionNet.loadFromUri("/models"),
+]).then(startLiveFeed);
 
-async function Compute() {
-  const FaceContainer = document.createElement("div");
-  FaceContainer.style.position = "relative";
-  ImageSection.append(FaceContainer);
-
-  const LabeledFaces = await LoadingIndividualTestImages();
-  console.log(LabeledFaces);
-
-  ImageSection.append("Loaded");
-
-  let image;
-  let FaceSquares;
-
-  UploadedImage.addEventListener("change", async () => {
-    if (image) {
-      image.remove();
-    }
-    if (FaceSquares) {
-      FaceSquares.remove();
-    }
-    image = await faceapi.bufferToImage(UploadedImage.files[0]);
-    FaceSquares = faceapi.createCanvasFromMedia(image);
-    ImageSection.append(image);
-    ImageSection.append(FaceSquares);
-    const theSizes = { width: image.width, height: image.height };
-    faceapi.matchDimensions(FaceSquares, theSizes);
-
-    const detected = await faceapi
-      .detectAllFaces(image)
-      .withFaceLandmarks()
-      .withFaceDescriptors();
-
-    const resizedSquares = faceapi.resizeResults(detected, theSizes);
-    resizedSquares.forEach((detected) => {
-      const square = detected.detection.box;
-      const drawSquares = new faceapi.draw.DrawBox(square, {
-        label: "Face",
-      });
-      drawSquares.draw(FaceSquares);
-    });
-  });
-}
-
-function LoadingIndividualTestImages() {
-  const names = [
-    "Angela Bassett",
-    "Danai Gurira",
-    "Letitia Wright",
-    "Lupita Nyong'o",
-  ];
-  return Promise.all(
-    names.map(async (label) => {
-      const Faces = [];
-      for (let i = 1; i <= 3; i++) {
-        const pics = faceapi.fetchImage(
-          "Access-Control-Allow-Origin:https://github.com/Khutso95/WSOA3028A_884200/tree/master/test_individial_images/${label}/${i}.jpg"
-        );
-        const detectedFaces = await faceapi
-          .detectSingleFace(pics)
-          .withFaceLandmarks()
-          .withFaceDescriptors();
-      }
-      return new faceapi.LabeledFaceDescriptors(label, Faces);
-    })
+function startLiveFeed() {
+  navigator.getUserMedia(
+    { video: {} },
+    (stream) => (myfeed.srcObject = stream),
+    (err) => console.error(err)
   );
 }
+
+myfeed.addEventListener(
+  "play",
+  () => {
+    const drawCanvas = faceapi.createCanvasFromMedia(myfeed);
+    document.body.append(drawCanvas);
+    const displaySize = { width: myfeed.width, height: myfeed.height };
+    faceapi.matchDimensions(drawCanvas, displaySize);
+    setInterval(async () => {
+      const FaceDetections = await faceapi
+        .detectAllFaces(myfeed, new faceapi.TinyFaceDetectorOptions())
+        .withFaceLandmarks()
+        .withFaceExpressions();
+      const resizeBoxes = faceapi.resizeResults(FaceDetections, displaySize);
+      drawCanvas
+        .getContext("2d")
+        .clearRect(0, 0, drawCanvas.width, drawCanvas.height);
+
+      faceapi.draw.drawDetections(drawCanvas, resizeBoxes);
+      faceapi.draw.drawFaceExpressions(drawCanvas, resizeBoxes);
+    });
+  },
+  100
+);
